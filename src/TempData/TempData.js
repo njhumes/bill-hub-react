0749e5570a84c29ea0badf918533b5e63908574d
Nicholas Humes <njhumes@gmail.com>
2019-03-01 10:27:56 -0700

    react front end billhub

---
+++ src/TempData/TempData.js
@@ -0,0 +1,91 @@
+// HAVE AN ASSORTMENT FROM DIFFERENT STATES
+const tempData = [];    
+const tempDataReps = [
+    {
+        "firstname": "Tammy",
+        "lastname": "Baldwin",
+        "state":"WI",
+        "party": "D",
+        "image": "tammy_baldwin.png" },
+    {
+        "firstname": "John",
+        "lastname": "Borasso",
+        "state":"WY",
+        "party": "R",
+        "image": "john_borasso.jpeg",},
+    {
+        "firstname": "Marsha",
+        "lastname": "Blackburn",
+        "state":"TN",
+        "party": "R",
+        "image": "marsha_blackburn.jpg",},
+    {
+        "firstname": "Make",
+        "lastname": "Braun",
+        "state":"IN",
+        "party": "R",
+        "image": "mike_braun.jpeg",},
+    {
+        "firstname": "Thomas",
+        "lastname": "Carper",
+        "state":"DE",
+        "party": "D",
+        "image": "thomas_carper.jpg",},
+    {
+        "firstname": "Sherrod",
+        "lastname": "Brown",
+        "state":"OH",
+        "party": "D",
+        "image": "sherrod_brown.png",},
+    {
+        "firstname": "Maria",
+        "lastname": "Cantwell",
+        "state":"WA",
+        "party": "D",
+        "image": "maria_cantwell.jpg",},
+    {
+        "firstname": "Benjamin",
+        "lastname": "Cardin",
+        "state":"MD",
+        "party": "D",
+        "image": "benjamin_cardin.jpg",},
+    {
+        "firstname": "Robert",
+        "lastname": "Casey",
+        "state":"PA",
+        "party": "D",
+        "image": "robert_casey.jpg",},
+    {
+        "firstname": "Kevin",
+        "lastname": "Cramer",
+        "state":"ND",
+        "party": "R",
+        "image": "kevin_cramer.jpeg",},
+    {
+        "firstname": "Ted",
+        "lastname": "Cruz",
+        "state":"TX",
+        "party": "R",
+        "image": "ted_cruz.jpg",},
+    {
+        "firstname": "Dianne",
+        "lastname": "Feinstein",
+        "state":"CA",
+        "party": "D",
+        "image": "dianne_feinstein.jpg",},
+    {
+        "firstname": "Deb",
+        "lastname": "Fischer",
+        "state":"D",
+        "party": "CA",
+        "image": "deb_fischer.jpg",},
+    {
+        "firstname": "Kirsten",
+        "lastname": "Gillibrand",
+        "state":"NY",
+        "party": "D",
+        "image": "kirsten_gillibrand.jpg",}
+];
+    
+exports.tempData = tempData;
+exports.tempDataReps = tempDataReps;
